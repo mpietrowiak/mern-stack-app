afdf2974d049be208891dca1503d5e18676046ae
Mateusz Pietrowiak <matas8@protonmail.com>
2020-05-02 22:43:01 +0200

    Finished REST API

--- src/controllers/books.controller.ts
+++ src/controllers/books.controller.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
 import IControllerBase from '../interfaces/IControllerBase.interface';
+import mongoose from 'mongoose';
 
 import Book from '../models/book.model';
 
@@ -16,72 +17,67 @@ class BooksController implements IControllerBase {
     this.router
       .get('/', this.listItems)
       .post('/', this.addItem)
-
       .get('/:id', this.getItem)
       .put('/:id', this.editItem)
       .delete('/:id', this.deleteItem);
   }
 
   listItems = async (req: Request, res: Response) => {
-    res.status(200).json({
-      msg: 'listItems'
-    });
-  }
-
-  addItem = async (req: Request, res: Response) => {
-    res.status(200).json({
-      msg: 'addItem'
-    });
+    const books = await Book.find({});
+    res.status(200).json(books);
   }
 
   getItem = async (req: Request, res: Response) => {
-    res.status(200).json({
-      msg: 'getItem'
-    });
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      res.status(422).json({ error: 'Please provide a proper ID.' });
+      return;
+    }
+    const book = await Book.findById(req.params.id);
+    if (book) {
+      res.status(200).json(book);
+    } else {
+      res.status(404).end();
+    }
+  }
+
+  addItem = async (req: Request, res: Response) => {
+    const { title, author } = req.body;
+    try {
+      await Book.create({
+        title,
+        author
+      });
+      res.status(201).end();
+    } catch (error) {
+      res.status(422).json({ error: error.toString() });
+    }
   }
 
   editItem = async (req: Request, res: Response) => {
-    res.status(200).json({
-      msg: 'listItems'
-    });
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      res.status(422).json({ error: 'Please provide a proper ID.' });
+      return;
+    }
+    const editedBook = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (editedBook) {
+      res.status(204).end();
+    } else {
+      res.status(404).end();
+    }
   }
 
   deleteItem = async (req: Request, res: Response) => {
-    res.status(200).json({
-      msg: 'editItem'
-    });
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      res.status(422).json({ error: 'Please provide a proper ID.' });
+      return;
+    }
+    const deletedBook = await Book.findByIdAndRemove(req.params.id, req.body);
+    if (deletedBook) {
+      res.status(204).end();
+    } else {
+      res.status(404).end();
+    } 
   }  
-
-  // handleGet = async (req: Request, res: Response) => {
-  //   console.log(req.params.id);
-  //   try {
-  //     if (req.params.id) {
-  //       const result = await Book.findById(req.params.id);
-  //       res.status(200).json(result);
-  //     } else {
-  //       const result = await Book.find({});
-  //       console.log(result);
-  //       res.status(200).json(result);
-  //     }
-  //   } catch (error) {
-  //     res.status(400).json({
-  //       error
-  //     });
-  //   }
-  // }
-
-  // handlePost = async (req: Request, res: Response) => {
-  //   const { title, author } = req.body;
-  //   try {
-  //     const newBook = new Book({ title, author });
-  //     await newBook.save();
-  //     res.status(200).json(newBook);
-  //   } catch (error) {
-  //     res.status(400).json({
-  //       error
-  //     });
-  //   }
-  // }
 }
 
 export default BooksController;
