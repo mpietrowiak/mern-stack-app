871143835247e44dd7f6fa81b679bd1b3e0c2195
Mateusz Pietrowiak <matas8@protonmail.com>
2020-05-03 13:23:13 +0200

    Fix TypeScript server watching, simplify next.js initialization code

--- src/controllers/frontend.controller.ts
+++ src/controllers/frontend.controller.ts
@@ -1,27 +1,28 @@
 import * as express from 'express';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import IControllerBase from '../interfaces/IControllerBase.interface';
-import path from 'path';
 import { parse } from 'url';
 import nextApp from '../nextapp';
 
 class FrontendController implements IControllerBase {
   public path = '/';
   public router = express.Router();
-  private nextHandler: any;
 
   constructor() {
     this.initRoutes();
-    this.nextHandler = nextApp.getRequestHandler();
   }
 
   public initRoutes() {
     this.router.get('*', this.index);
   }
 
-  index = (req: Request, res: Response) => {
-    const { pathname, query } = parse(req.url, true);
-    this.nextHandler(req, res, pathname, query);
+  index = (req: Request, res: Response, next: NextFunction ) => {
+    const { query } = parse(req.url, true);
+    try {
+      nextApp.render(req, res, '/', query);
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
