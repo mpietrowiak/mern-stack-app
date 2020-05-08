3dd142f5753e855e3706fc926716a9e7054940ac
Mateusz Pietrowiak <matas8@protonmail.com>
2020-05-08 13:56:18 +0200

    Add header margin for better looks

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <nav>
+    <nav style={{marginBottom:'30px'}}>
       <Link to="/">Home</Link> &nbsp;| &nbsp;
       <Link to="/user">User profile</Link> &nbsp;| &nbsp;
       <Link to="/cookies">Cookies demo</Link>      
