91590f73af6d28dce5428f44f98a5aa5c2eed648
Mateusz Pietrowiak <matas8@protonmail.com>
2020-05-12 12:49:43 +0200

    Remove console.logs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-console.log('next config ........');
-
 module.exports = {
   webpack: (config) => {
-    console.log(__dirname);
     config.resolve.alias['@'] = __dirname;
-    console.log(config.resolve);
     return config;
   }
 }
